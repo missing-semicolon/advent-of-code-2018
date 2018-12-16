56f15896de8ac7a31a714dd634b0787700842854
missing-semicolon <s.j.lustig@gmail.com>
2018-12-16 13:20:14 -0500

    Solve part 1

--- day09_marble_mania.py
+++ day09_marble_mania.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 """
 0, (1), 2
@@ -17,20 +17,23 @@ from typing import List
 3 + 2 % 4 = 1
 """
 
-def play_game(num_players: int, highest_marble: int) -> int:
-    circle = [0]
-    current_marble = 0
+
+def play_game_old(num_players: int, highest_marble: int) -> int:
+    circle: List[int] = [0]
+    current_marble: int = 0
+    scoreboard: Dict[int, int] = {x: 0 for x in range(num_players + 1)}
 
     for marble in range(highest_marble + 1):
         if marble == 0:
             continue
 
         player = marble % num_players
+        if player == 0:
+            player = num_players
         if marble % 23 != 0:
             if len(circle) == 1:
                 circle = circle + [marble]
                 current_marble = 1
-                print(player, circle)
                 continue
             break_point_left = (current_marble + 1) % len(circle)
             if break_point_left == len(circle) - 1:
@@ -38,8 +41,30 @@ def play_game(num_players: int, highest_marble: int) -> int:
                 current_marble = break_point_left + 1
             else:
                 break_point_right = (current_marble + 2) % len(circle)
-                circle = circle[:break_point_left + 1] + [marble] + circle[(break_point_right):]
+                circle = circle[:break_point_left + 1] + \
+                    [marble] + circle[(break_point_right):]
                 current_marble = break_point_right
-            print(player, circle, circle[current_marble])
+        else:
+            scoreboard[player] += marble
+            take_index = (current_marble - 7) % len(circle)
+            take_marble = circle[take_index]
+            scoreboard[player] += take_marble
+            circle.remove(take_marble)
+            current_marble = take_index
+    return max([v for v in scoreboard.values()])
 
-play_game(9, 22)
+
+# assert play_game(9, 25) == 32
+# assert play_game(10, 1618) == 8317
+# assert play_game(13, 7999) == 146373
+# assert play_game(17, 1104) == 2764
+# assert play_game(21, 6111) == 54718
+# assert play_game(30, 5807) == 37305
+
+# 459 players; last marble is worth 72103 points
+print(play_game_old(459, 72103))
+
+# print(play_game(459, (459 * 23 * 1) + 21))
+# print(play_game(459, (459 * 23 * 2) + 21))
+
+# print(play_game(459, 72103 * 100))
